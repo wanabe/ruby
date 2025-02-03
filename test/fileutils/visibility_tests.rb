f2463d497e61d025f3b418ceff1223bca8da0b09
wanabe <s.wanabe@gmail.com>
2025-02-03 12:51:22 +0900

    test/fileutils

--- test/fileutils/visibility_tests.rb
+++ test/fileutils/visibility_tests.rb
@@ -9,31 +9,35 @@ require 'fileutils'
 module TestFileUtilsIncVisibility
 
   FileUtils::METHODS.each do |m|
-    define_method "test_singleton_visibility_#{m}" do
-      assert @fu_module.respond_to?(m, true),
-             "FileUtils::Verbose.#{m} is not defined"
-      assert @fu_module.respond_to?(m, false),
-             "FileUtils::Verbose.#{m} is not public"
-    end
+    eval <<~EOS
+      def test_singleton_visibility_#{m}
+        assert @fu_module.respond_to?(:#{m}, true),
+              "FileUtils::Verbose.#{m} is not defined"
+        assert @fu_module.respond_to?(:#{m}, false),
+              "FileUtils::Verbose.#{m} is not public"
+      end
 
-    define_method "test_visibility_#{m}" do
-      assert respond_to?(m, true),
-             "FileUtils::Verbose\##{m} is not defined"
-      assert @fu_module.private_method_defined?(m),
-             "FileUtils::Verbose\##{m} is not private"
-    end
+      def test_visibility_#{m}
+        assert respond_to?(:#{m}, true),
+              "FileUtils::Verbose\##{m} is not defined"
+        assert @fu_module.private_method_defined?(:#{m}),
+              "FileUtils::Verbose\##{m} is not private"
+      end
+    EOS
   end
 
   FileUtils::StreamUtils_.private_instance_methods.each do |m|
-    define_method "test_singleton_visibility_#{m}" do
-      assert @fu_module.respond_to?(m, true),
-             "FileUtils::Verbose\##{m} is not defined"
-    end
+    eval <<~EOS
+      def test_singleton_visibility_#{m}
+        assert @fu_module.respond_to?(:#{m}, true),
+              "FileUtils::Verbose\##{m} is not defined"
+      end
 
-    define_method "test_visibility_#{m}" do
-      assert respond_to?(m, true),
-             "FileUtils::Verbose\##{m} is not defined"
-    end
+      def test_visibility_#{m}
+        assert respond_to?(:#{m}, true),
+              "FileUtils::Verbose\##{m} is not defined"
+      end
+    EOS
   end
 
 end
